0e813f7ef402528eddfb74281e145d3f9b296a87
Lucas da Silva Leoncio <47578616+lucasdksan@users.noreply.github.com>
2020-07-20 20:37:18 -0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,71 @@
+import api from './api';
+
+class app{
+    constructor(){
+        this.repositories = [];
+        this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
+        this.listEl = document.getElementById('repo-list');
+        this.registerHandlers();
+    }
+    registerHandlers(){
+        this.formEl.onsubmit = (event) =>{
+            this.addRepository(event);
+        }
+    }
+    setLoading(loading = true){
+        if(loading === true){
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando'));
+            loadingEl.setAttribute('id','loading');
+            this.formEl.appendChild(loadingEl);
+        } else{
+            document.getElementById('loading').remove();
+        }
+    }
+    async addRepository(event){
+        event.preventDefault();
+        const repoInput = this.inputEl.value;
+        if(repoInput.length === 0){
+            return;
+        }
+        this.setLoading();
+        try{
+            const response = await api.get(`/repos/${repoInput}`);
+            const {name,description,html_url,owner:{avatar_url}} = response.data; 
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url
+            });
+            this.inputEl = '';
+            this.render();
+        }catch(err){
+
+        }
+        this.setLoading(false);
+    }
+    render(){
+        this.listEl.innerHTML = '';
+        this.repositories.forEach(repo=>{
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src',repo.avatar_url);
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target','_blank');
+            linkEl.setAttribute('href',repo.html_url)
+            linkEl.appendChild(document.createTextNode('Acessar'));
+            let listItensEl = document.createElement('li');
+            listItensEl.appendChild(imgEl);
+            listItensEl.appendChild(titleEl);
+            listItensEl.appendChild(descriptionEl);
+            listItensEl.appendChild(linkEl);
+            this.listEl.appendChild(listItensEl);
+        });
+    }
+}
+new app();
